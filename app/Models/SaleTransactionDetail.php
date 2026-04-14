bd4b6cd17b2fef915751a0d5fecdcf4db4dbd5ae
alif firdiansyah <aliffirdiansyah5@gmail.com>
2026-04-14 10:56:07 +0700

    info pizza hut

--- app/Models/SaleTransactionDetail.php
+++ app/Models/SaleTransactionDetail.php
@@ -36,7 +36,7 @@ class SaleTransactionDetail extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class, 'product_id');
+        return $this->belongsTo(Product::class, 'product_id')->withTrashed();
     }
 
     // User tracking
